44ed9f52cf36af8f845dbe16b0fd9beb09363a4e
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-02-03 15:06:40 +0100

    initial working version readOdyssee

--- R/readOdyssee.R
+++ R/readOdyssee.R
@@ -7,20 +7,73 @@
 #' @param subtype type of result (\code{enduseShares} or \code{carrierShares})
 #' @returns data.frame with historic enduse or carrier shares
 #'
-#' @author Pascal Führlich, Antoine Levesque
+#' @author Antoine Levesque, Pascal Führlich
 #'
 #' @references Enerdata. Odyssee-Mure database. https://www.odyssee-mure.eu/
-#' @importFrom countrycode countrycode
-#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate filter rename left_join select group_by summarise ungroup across %>%
+#' @importFrom madrat toolCountry2isocode
+#' @importFrom quitte revalue.levels interpolate_missing_periods aggregate_map getRegs
 #' @importFrom rlang .data syms
-#' @importFrom dplyr mutate filter rename left_join select group_by summarise ungroup across
 #' @importFrom tidyr replace_na complete separate
 #' @importFrom tidyselect all_of one_of
-#' @importFrom quitte revalue.levels interpolate_missing_periods aggregate_map getRegs
-readOdyssee <- function(subtype = "enduseShares") {
+readOdyssee <- function(subtype = c("enduseShares", "carrierShares")) {
+  subtype <- match.arg(subtype)
+
+  # TODO ensure input files are available on cluster
   services <- read.csv("export_enerdata_7793_105638_services.csv", skip = 1, na.strings = c("n.a.", ""))
   households <- read.csv("export_enerdata_7793_105710_households.csv", skip = 1, na.strings = c("n.a.", ""))
+  odyssee <- rbind(households, services)
+  colnames(odyssee) <- c("variable", "region", "unit", "period", "value", "note", "title")
+  # TODO return magpie
+
+  regionalMap <- readRDS("regionalmap.rds")  # TODO proper regionalmapping?
+
+  # TODO create convert
   regionalTargetDimension <- "EDGE_EUR_ETP" # TODO correct? previously: getOption("regional_target_dimension")
+  regionalMap <- na.omit(regionalMap[c("iso", regionalTargetDimension)])
+
+  # ISO3 country codes, unit conversion and gap filling
+  unit2EJ <- c(Mtoe = 4.1868E-2, ktoe = 4.1868E-5, Tj = 1E-6)
+  odyssee <- odyssee %>%
+    filter(!is.na(.data[["value"]])) %>%
+    mutate(region = toolCountry2isocode(.data[["region"]]),
+           value = .data[["value"]] * unit2EJ[.data[["unit"]]],
+           unit = "EJ") %>%
+    select(-"note", -"title") %>%
+    interpolate_missing_periods(expand.values = TRUE)
+
+  # aggregate to EDGE regions
+  revalCarrier <- c(
+    cms = "coal",
+    pet = "petrol",
+    gaz = "natgas",
+    vap = "heat",
+    enc = "biomod",
+    elc = "elec",
+    toc = "total")
+  revalSector <- c(
+    cfres = "residential",
+    cfter = "services")
+  revalEnduse <- c(
+    chf = "space_heating",
+    ecs = "water_heating",
+    chc = "space_heating_corrected",
+    cui = "cooking",
+    cli = "space_cooling",
+    els = "appliances_light",
+    els1 = "appliances",
+    lgt = "lighting")
+  redundantEnduse <- c("space_heating_corrected", "appliances_light")
+  redundantCarrier <- "total"
+  odyssee <- odyssee %>%
+    aggregate_map(filter(regionalMap, .data[["iso"]] %in% getRegs(odyssee)),
+                  by = c(region = "iso")) %>%
+    separate("variable", c("carrier", "sector", "enduse"), c(3, 8)) %>%
+    revalue.levels(carrier = revalCarrier,
+                   sector  = revalSector,
+                   enduse  = revalEnduse) %>%
+    filter(!.data[["enduse"]]  %in% redundantEnduse,
+           !.data[["carrier"]] %in% redundantCarrier)
 
   computeSharesAndComplete <- function(df, .groups) {
     grReg <- c("region", "period", .groups)
@@ -57,61 +110,6 @@ readOdyssee <- function(subtype = "enduseShares") {
       calcShares(colShares)
   }
 
-  regionalMap <- na.omit(readRDS("regionalmap.rds")[c("iso", regionalTargetDimension)]) # TODO proper regionalmap?
-
-  revalCarrier <- c(
-    cms = "coal",
-    pet = "petrol",
-    gaz = "natgas",
-    vap = "heat",
-    enc = "biomod",
-    elc = "elec",
-    toc = "total")
-
-  revalSector <- c(
-    cfres = "residential",
-    cfter = "services")
-
-  revalEnduse <- c(
-    chf = "space_heating",
-    ecs = "water_heating",
-    chc = "space_heating_corrected",
-    cui = "cooking",
-    cli = "space_cooling",
-    els = "appliances_light",
-    els1 = "appliances",
-    lgt = "lighting")
-
-  unit2EJ <- c(Mtoe = 4.1868E-2, ktoe = 4.1868E-5, Tj = 1E-6)
-  redundantEnduse <- c("space_heating_corrected", "appliances_light")
-  redundantCarrier <- "total"
-
-
-  # ==== PROCESS DATA ==========================
-  odyssee <- rbind(households, services)
-  colnames(odyssee) <- c("variable", "region", "unit", "period", "value",
-                         "note", "title")
-
-  # ISO3 country codes, unit conversion and gap filling
-  odyssee <- odyssee %>%
-    filter(!is.na(.data[["value"]])) %>%
-    mutate(region = countrycode(.data[["region"]], "iso2c", "iso3c"), # TODO possible without countrycode?
-           value = .data[["value"]] * unit2EJ[.data[["unit"]]],
-           unit = "EJ") %>%
-    select(-"note", -"title") %>%
-    interpolate_missing_periods(expand.values = TRUE)
-
-  # aggregate to EDGE regions,
-  odyssee <- odyssee %>%
-    aggregate_map(filter(regionalMap, .data[["iso"]] %in% getRegs(odyssee)),
-                  by = c(region = "iso")) %>%
-    separate("variable", c("carrier", "sector", "enduse"), c(3, 8)) %>%
-    revalue.levels(carrier = revalCarrier,
-                   sector  = revalSector,
-                   enduse  = revalEnduse) %>%
-    filter(!.data[["enduse"]]  %in% redundantEnduse,
-           !.data[["carrier"]] %in% redundantCarrier)
-
   switch(subtype,
          enduseShares = {
            odyssee <- odyssee %>%
@@ -124,20 +122,17 @@ readOdyssee <- function(subtype = "enduseShares") {
              mutate(value = replace_na(.data[["value"]], 0)) # for end uses which are fully electric
            biotrad <- odyssee %>%
              filter(.data[["carrier"]] == "biomod") %>%
-             mutate(value = 0,
-                    carrier = "biotrad")
+             mutate(value = 0, carrier = "biotrad")
            odyssee <- rbind(odyssee, biotrad)
          },
-         stop(paste("Invalid subtype:", subtype))
-  )
+         stop(paste("Invalid subtype:", subtype)))
 
-  return(as.magpie(odyssee)) # TODO conversion as.magpie correct?
+  return(as.magpie(odyssee))
 }
 
 
 #' @importFrom lazyeval interp
-#' @importFrom dplyr mutate_ ungroup group_by ungroup across
-#' @importFrom tidyr %>%
+#' @importFrom dplyr mutate_ ungroup group_by ungroup across %>%
 #' @importFrom stats setNames
 #' @importFrom tidyselect all_of
 #'
