e294cd633ba7c0425235d2aa8e0953075171b60b
krekeler <robin.krekeler@pik-potsdam.de>
2022-03-21 11:44:39 +0100

    Process Odyssee data base.

--- R/readOdyssee.R
+++ R/readOdyssee.R
@@ -1,22 +1,50 @@
 #' Read Odyssee data base
 #'
-#' Read and tidy data on final energy consumption for different end uses in
-#' residential (households) and commercial (services) buildings in Europe.
-#' Calculates end use or carrier shares for European EDGE regions.
+#' Copied from website: The Odyssee database contains detailed energy
+#' consumption by end-use and their drivers as well as energy efficiency and
+#' CO2-related indicators. Latest available data is provided by national
+#' representatives, such as energy agencies or statistical organization, from
+#' all EU countries as well as Norway, Serbia, Switzerland and the United
+#' Kingdom.
 #'
-#' @returns data.frame with historic enduse or carrier shares
+#' @source https://odyssee.enerdata.net/database/
+#' @note To download new data, log into the website, select all items of all
+#' levels and download the data 'column-orientated csv'.
+#' @note  Variables are labels with the item code but full names can be found in
+#' the source data
 #'
-#' @author Antoine Levesque, Pascal Führlich
+#' @param subtype database category
+#' @returns magpie object
 #'
-#' @references Enerdata. Odyssee-Mure database. https://www.odyssee-mure.eu/
+#' @author Pascal Führlich, Robin Krekeler
 #'
-#' @importFrom madrat toolCountry2isocode
-readOdyssee <- function() {
-  services <- read.csv("export_enerdata_7793_105638_services.csv", skip = 1, na.strings = c("n.a.", ""))
-  households <- read.csv("export_enerdata_7793_105710_households.csv", skip = 1, na.strings = c("n.a.", ""))
-  odyssee <- rbind(households, services)
-  colnames(odyssee) <- c("variable", "region", "unit", "year", "value", "note", "title")
-  odyssee$region <- toolCountry2isocode(odyssee$region)
-  odyssee$year <- paste0("y", odyssee$year)
-  return(as.magpie(odyssee))
+#' @importFrom utils read.csv
+#' @importFrom dplyr %>% select mutate
+#' @importFrom rlang .data
+#' @importFrom tidyr unite
+#' @importFrom quitte as.quitte
+#' @importFrom magclass as.magpie
+#' @export
+
+readOdyssee <- function(subtype = "households") {
+
+  # check subtype
+  file <- switch(subtype,
+                 households = "export_enerdata_7793_105710_households.csv",
+                 services   = "export_enerdata_7793_105638_services.csv",
+                 stop("'", subtype, "' is not a valid subtype."))
+
+  # read data
+  data <- read.csv(file, skip = 1, na.strings = c("n.a.", "")) %>%
+    select(region = "ISO.code",
+           period = "Year",
+           variable = "Item.code",
+           value = "Value",
+           unit = "Unit") %>%
+    mutate(value = as.numeric(.data[["value"]])) %>%
+    unite("variable", "variable", "unit", na.rm = TRUE) %>%
+    as.quitte() %>%
+    as.magpie()
+
+  return(data)
 }
