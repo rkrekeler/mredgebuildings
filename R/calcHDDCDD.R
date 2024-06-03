9d22ffc46949ff4d5a68d9e2871cb24970062375
hagento <hagen.tockhorn@pik-potsdam.de>
2024-06-03 13:36:45 +0200

    removed checkTDif from compHDDCDDFactors

--- R/calcHDDCDD.R
+++ R/calcHDDCDD.R
@@ -589,23 +589,6 @@ compHDDCDDFactors <- function(tlow, tup, tlim, tambStd = 5, tlimStd = 5) {
     return(h)
   }
 
-  # check if ambient/limit temperature interval is reasonable
-  # e.g. tLim = 17C and t_amb = -50C wouldn't give reasonable CDD
-  checkTDif <- function(tamb, tlim, typeDD, tambStd, tlimStd) {
-    check <- TRUE
-    stdDif <- tambStd + tlimStd
-    if (typeDD == "HDD") {
-      if (tamb - tlim > stdDif) {
-        check <- FALSE
-      }
-    } else if (typeDD == "CDD") {
-      if (tlim - tamb > stdDif) {
-        check <- FALSE
-      }
-    }
-    return(check)
-  }
-
   t <- seq(tlow, tup, .1)
 
   hddcddFactors <- do.call(
@@ -617,48 +600,42 @@ compHDDCDDFactors <- function(tlow, tup, tlim, tambStd = 5, tlimStd = 5) {
               do.call(
                 "rbind", lapply(
                   tlim[[typeDD]], function(.tlim) {
-                    if (!checkTDif(tamb, .tlim, typeDD, tambStd, tlimStd)) {
-                      tmp <- data.frame("T_amb"        = tamb,
-                                        "T_amb_K"      = round(tamb + 273.15, 1),
-                                        "tLim"         = .tlim,
-                                        "factor"       = 0,
-                                        "factor_err"   = 0,
-                                        "typeDD"       = typeDD)
-                    } else {
-                      # tlim integration boundaries
-                      x1 <- .tlim - 3*tlimStd
-                      x2 <- .tlim + 3*tlimStd
 
-                      if (typeDD == "HDD") {
-                        f <- integral2(heatingFactor,
-                                       xmin = x1,
-                                       xmax = x2,
-                                       ymin = tamb - 3*tambStd,
-                                       ymax = min(.tlim, tamb + 3*tambStd),
-                                       tamb = tamb,
-                                       tambStd = tambStd,
-                                       tlim = .tlim,
-                                       tlimStd = tlimStd,
-                                       reltol = 1e-1)
-                      } else {
-                        f <- integral2(coolingFactor,
-                                       xmin = x1,
-                                       xmax = x2,
-                                       ymin = max(.tlim, tamb - 3*tambStd),
-                                       ymax = tamb + 3*tambStd,
-                                       tamb = tamb,
-                                       tambStd = tambStd,
-                                       tlim = .tlim,
-                                       tlimStd = tlimStd,
-                                       reltol = 1e-1)
-                      }
-                      tmp <- data.frame("T_amb"        = tamb,
-                                        "T_amb_K"      = round(tamb + 273.15, 1),
-                                        "tLim"         = .tlim,
-                                        "factor"       = f$Q,
-                                        "factor_err"   = f$error,
-                                        "typeDD"       = typeDD)
+                    # tlim integration boundaries
+                    x1 <- .tlim - 3*tlimStd
+                    x2 <- .tlim + 3*tlimStd
+
+                    if (typeDD == "HDD") {
+                      f <- integral2(heatingFactor,
+                                     xmin = x1,
+                                     xmax = x2,
+                                     ymin = tamb - 3*tambStd,
+                                     ymax = min(.tlim, tamb + 3*tambStd),
+                                     tamb = tamb,
+                                     tambStd = tambStd,
+                                     tlim = .tlim,
+                                     tlimStd = tlimStd,
+                                     reltol = 1e-1)
+                    } else {
+                      f <- integral2(coolingFactor,
+                                     xmin = x1,
+                                     xmax = x2,
+                                     ymin = max(.tlim, tamb - 3*tambStd),
+                                     ymax = tamb + 3*tambStd,
+                                     tamb = tamb,
+                                     tambStd = tambStd,
+                                     tlim = .tlim,
+                                     tlimStd = tlimStd,
+                                     reltol = 1e-1)
                     }
+
+                    tmp <- data.frame("T_amb"        = tamb,
+                                      "T_amb_K"      = round(tamb + 273.15, 1),
+                                      "tLim"         = .tlim,
+                                      "factor"       = f$Q,
+                                      "factor_err"   = f$error,
+                                      "typeDD"       = typeDD)
+
                     return(tmp)
                   }
                 )
